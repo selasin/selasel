82a6724a15999cd14ada24d818caaeb234663862
selasin <91956682+selasin@users.noreply.github.com>
2021-10-05 14:39:46 +0200

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,41 @@
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery-container img");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+
+previews.forEach(preview => {
+  preview.addEventListener("click", () => {
+    modal.classList.add("open");
+    original.classList.add("open");
+    //Dynamic change text and image://
+    const originalSrc = preview.getAttribute("data-original");
+    original.src = originalSrc;
+    const altText = preview.alt;
+    caption.textContent = altText;
+
+  })
+})
+
+modal.addEventListener ("click", (e) => {
+    if(e.target.classList.contains("modal")) {
+      modal.classList.remove("open");
+      original.classList.remove("open");
+
+    }
+});
+
+
+
+
+
+/* PREVIOUS MODAL CODE: let galleryImages = document.querySelectorAll(".gallery-image");
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+if(galleryImages) {
+    galleryImages.forEach(function(image) {
+      image.onclick = function() {
+      let getElementCss = window.getComputedStyle(image);
+      let getFullImgUrl = getElementCss.getPropertyValue ("background-image");
+    });
+} */
